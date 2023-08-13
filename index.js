1488ab63e932c6c256bdd088b0dda03620e5e5c3
nayem islam <gangsn490@gmail.com>
2023-08-13 18:43:36 +0600

    Add new item

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const anyArray = [1, 2, 3];
+const toString = Object.prototype.toString;
 
-const result = anyArray.toString(); // "1,2,3"
+toString.call(new Date()); // [object Date]
+toString.call(new String()); // [object String]
+// Math has its Symbol.toStringTag
+toString.call(Math); // [object Math]
+
+const result = toString.call(undefined); // [object Undefined]
+toString.call(null); // [object Null]
 
 console.log(result);
